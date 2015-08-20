77fb48396f73bab336d3a61a4939b5c33f62990c
Jordi Deu-Pons <jordi.deu@upf.edu>
2015-08-20 10:53:09 +0200

    Next release 0.4.0

--- itab/__init__.py
+++ itab/__init__.py
@@ -3,7 +3,7 @@ from itab.writer import TabWriter, TabDictWriter
 
 __author__ = 'Jordi Deu-Pons'
 __author_email__ = 'jordi@jordeu.net'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 
 def has_schema(file):
